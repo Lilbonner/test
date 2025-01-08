b768bc087eb1b5345c2e88f9a00cd25f5c5a3aec
Daniyar Nasirov <dannasirov312@gmail.com>
2025-01-08 14:07:06 +0600

    created some movie card, with importing api data

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+import { Card, CardMedia, CardContent, Typography, Button, CardActionArea, CardActions } from '@mui/material';
+
+const MovieCard = ({ movie }: { movie: any }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(`/movie/${movie.imdbID}`);
+    };
+
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea onClick={handleClick}>
+                <CardMedia
+                    component="img"
+                    height="100%"
+                    image={movie.Poster}
+                    alt={movie.Title}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {movie.Title}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        {movie.Year}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    add to favorite
+                </Button>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default MovieCard;

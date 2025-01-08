fa8f2b114df549196fa97e2bbb31a17e2ead95f4
Daniyar Nasirov <dannasirov312@gmail.com>
2025-01-08 14:10:51 +0600

    added router for app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import './App.css'
+import './App.css';
+import SearchBar from './components/SearchBar.tsx';
+import MovieList from './components/MovieList';
+import MovieDetails from './components/MovieDetail';
+import FavoriteList from './components/FavoriteList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <Router>
+            <div className="App">
+                <SearchBar />
+                <Routes>
+                    <Route path="/" element={<MovieList />} />
+                    <Route path="/movie/:id" element={<MovieDetails />} />
+                    <Route path="/favorites" element={<FavoriteList />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
